d7ac83c184f7e72612d0fe0db07e3dcd331901bb
xuziqii <life-record@qq.com>
2020-02-08 17:31:02 +0800

    add: invoice dash

---
+++ src/components/InvoiceDash.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="dash-wrapper">
+    <div class="dot left"></div>
+    <div class="dash-line">
+    </div>
+    <div class="dot right"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InvoiceDash',
+}
+</script>
+<style lang="stylus" scoped>
+defaultWidth = 8px
+
+.dash-wrapper
+  display flex
+  align-items center
+  background #fff
+  .dot
+    width defaultWidth
+    height defaultWidth * 2
+    background #f5f5f5
+    &.left
+      border-radius 0 defaultWidth defaultWidth 0
+    &.right
+      border-radius defaultWidth 0 0 defaultWidth
+  .dash-line
+    flex 1
+    margin 0 defaultWidth
+    border-top 1px dashed #eee
+</style>
