763b8543531acb3f17744aeef9358523545f2d9f
xuziqii <life-record@qq.com>
2020-02-08 17:31:13 +0800

    add: invoice head

---
+++ src/components/InvoiceHead.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="wrapper">
+      <div class="text">{{ text }}</div>
+      <div class="big">
+        <div class="small"></div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'InvoiceHead',
+  props: {
+    text: {
+      type: String,
+      default: '增值税普通发票(电子)',
+    },
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+defaultColor = #F58A88
+defaultBorderRadius = 60%
+
+.wrapper
+  display flex
+  flex-direction column
+  align-items center
+  position relative
+  .text
+    position absolute
+    top 21px
+    color defaultColor
+    font-size 18px
+  .big
+    position relative
+    border-radius defaultBorderRadius
+    height 60px
+    width 100px
+    border 2px solid defaultColor
+    .small
+      position absolute
+      left 4px
+      top 4px
+      border-radius defaultBorderRadius
+      height 50px
+      width 90px
+      border 1px solid defaultColor
+</style>
